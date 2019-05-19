a251fd6b90ab1ea263d2a419118a2393e671ff66
Janchorizo <jancho@usal.es>
2019-05-19 17:36:25 +0200

    Change main task.

--- pipeline/run.py
+++ pipeline/run.py
@@ -5,17 +5,29 @@ from os import path
 from tasks.utils import MetaOutputHandler
 from tasks.utils import GlobalParams
 # Pipeline task module classes
-from tasks.reference import ReferenceGenome
+from tasks.vcf import VariantCalling
 
 class Pipeline(luigi.WrapperTask):
     def requires(self):
-        return ReferenceGenome()
+        return VariantCalling()
 
 if __name__ == '__main__':
     luigi.run(['Pipeline', 
-            '--workers', '2',
+            '--workers', '3', 
+            '--VariantCalling-use_platypus', 'true',
+            '--VariantCalling-use_freebayes', 'true',
+            '--VariantCalling-use_samtools', 'false',
+            '--VariantCalling-use_gatk', 'false',
+            '--VariantCalling-use_deepcalling', 'false',
+            '--AlignProcessing-cpus', '6',
+            '--FastqAlign-cpus', '6', 
+            '--FastqAlign-create-report', 'True', 
+            '--GetFastq-fastq1-url', '',
+            '--GetFastq-fastq1-url', '',
+            '--GetFastq-fastq1-from-ebi', 'False',
+            '--GetFastq-fastq1-paired-end', 'True',
             '--ReferenceGenome-ref-url', 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.2bit',
             '--ReferenceGenome-from2bit', 'True',
             '--GlobalParams-base-dir', './experiment',
             '--GlobalParams-log-dir', path.abspath(path.curdir),
-            '--GlobalParams-exp-name', 'get_ref_genome'])
+            '--GlobalParams-exp-name', 'hg19'])
